68d9fd4fadcbd919bfdc0cdd399f8732163b79db
Ilya Korab <ilya-korob@yandex.ru>
2023-08-09 22:18:49 +0200

    add project skeleton

---
+++ robotyka/src/main/java/com/ilyakorob/robotyka/RobotykaApplication.java
@@ -0,0 +1,13 @@
+package com.ilyakorob.robotyka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotykaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotykaApplication.class, args);
+	}
+
+}
---
+++ robotyka/src/test/java/com/ilyakorob/robotyka/RobotykaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilyakorob.robotyka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotykaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
