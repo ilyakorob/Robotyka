37b111e83edc7104c3f35728afa941fe40ddaf64
Ilya Korab <ilya-korob@yandex.ru>
2023-10-19 22:15:36 +0200

    add relations between entities

--- robotyka/src/main/java/com/ilyakorob/robotyka/models/Teacher.java
+++ robotyka/src/main/java/com/ilyakorob/robotyka/models/Teacher.java
@@ -1,9 +1,32 @@
 package com.ilyakorob.robotyka.models;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Teacher")
 public class Teacher {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotEmpty(message = "Pole nie może być puste ")
+    @Size(min = 2, max = 100, message = "Imie musi mieć od 2 do 100 znaków")
+    @Column(name = "name")
     private String name;
+    @NotEmpty(message = "Pole nie może być puste ")
+    @Size(min = 2, max = 100, message = "Nazwisko musi mieć od 2 do 100 znaków")
+    @Column(name = "surname")
     private String surname;
+
+    @OneToMany(mappedBy = "teacher")
+    @JoinColumn(name = "teacher_id")
+    private List<Student> students;
 
     public Teacher() {
 
